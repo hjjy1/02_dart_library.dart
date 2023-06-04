7d01db5d52cb77f860b78d8a95aeac5cdc41906a
航 蒋 <244491988@qq.com>
2023-06-04 18:47:19 +0000

    add

---
+++ 02_Control_flow_statements.dart
@@ -0,0 +1,24 @@
+void main() {
+var year = DateTime.now().year;
+
+  if (year >= 2001) {
+    print('21s centry');
+  } else if (year >= 1901) {
+    print('20th centry');
+  }
+
+  var flybyObjects = ['a', 'b', 'c'];
+  for (final object in flybyObjects) {
+    print(object);
+  }
+
+  for (int month = 1; month <= 12; month++) {
+    print(month);
+  }
+
+  year = 2009;
+  while (year < 2016) {
+    year += 1;
+    print(year);
+  }
+}
